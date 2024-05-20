66c667ce3d9b1c4ac6c498f423c193432f93c7af
bluurryy <164359728+bluurryy@users.noreply.github.com>
2024-05-20 22:33:07 +0200

    doc: mention that `NoDrop` doesn't catch all types that don't need to be dropped (#22)

--- src/lib.rs
+++ src/lib.rs
@@ -374,6 +374,8 @@ const DEFAULT_START_CHUNK_SIZE: usize = 512;
 
 /// This trait marks types that don't need dropping.
 ///
+/// This trait is a best effort for modeling such a constraint. It is not implemented for all types that don't need dropping.
+///
 /// It is used as a bound for [`BumpBox`]'s [`into_ref`](BumpBox::into_ref) and [`into_mut`](BumpBox::into_mut) so you don't accidentally omit a drop that does matter.
 pub trait NoDrop {}
 
