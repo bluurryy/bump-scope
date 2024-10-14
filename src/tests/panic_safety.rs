3486ef4665bc673ca33952fab782cf82c40ec1b2
bluurryy <164359728+bluurryy@users.noreply.github.com>
2024-10-14 20:28:39 +0200

    test: refactor panic safety tests

--- src/tests/panic_safety.rs
+++ src/tests/panic_safety.rs
@@ -21,13 +21,13 @@ macro_rules! zst_or_not {
                 #[test]
                 #[cfg_attr(not(panic = "unwind"), ignore = "test requires unwinding support")]
                 fn non_zst() {
-                    super::$name::<i32>();
+                    super::$name::<super::helper::Wrap<i32>>();
                 }
 
                 #[test]
                 #[cfg_attr(not(panic = "unwind"), ignore = "test requires unwinding support")]
                 fn zst() {
-                    super::$name::<()>();
+                    super::$name::<super::helper::Wrap<()>>();
                 }
             }
         )*
@@ -49,48 +49,42 @@ zst_or_not! {
 }
 
 fn init_clone<T: Testable>() {
-    cfg().max_clones(3).expected_drops(3).run(|| {
-        let original = ManuallyDrop::new([
-            Wrap(T::default()),
-            Wrap(T::default()),
-            Wrap(T::default()),
-            Wrap(T::default()),
-            Wrap(T::default()),
-        ]);
+    expected_drops(3).panic_on_clone(3).run(|| {
+        let original = ManuallyDrop::new(T::array::<5>());
         let bump: Bump = Bump::new();
-        let uninit = bump.alloc_uninit_slice::<Wrap<T>>(5);
+        let uninit = bump.alloc_uninit_slice::<T>(5);
         let _init = uninit.init_clone(&*original);
     });
 }
 
 fn init_fill<T: Testable>() {
-    cfg().max_clones(3).expected_drops(4).run(|| {
+    expected_drops(4).panic_on_clone(3).run(|| {
         let bump: Bump = Bump::new();
-        let uninit = bump.alloc_uninit_slice::<Wrap<T>>(5);
-        let _init = uninit.init_fill(Wrap(T::default()));
+        let uninit = bump.alloc_uninit_slice::<T>(5);
+        let _init = uninit.init_fill(T::default());
     });
 }
 
 fn init_fill_with<T: Testable>() {
-    cfg().max_clones(3).expected_drops(3).run(|| {
-        let original = ManuallyDrop::new(Wrap(T::default()));
+    expected_drops(3).panic_on_clone(3).run(|| {
+        let original = ManuallyDrop::new(T::default());
         let bump: Bump = Bump::new();
-        let uninit = bump.alloc_uninit_slice::<Wrap<T>>(5);
+        let uninit = bump.alloc_uninit_slice::<T>(5);
         let _init = uninit.init_fill_with(|| (*original).clone());
     });
 }
 
 fn mut_bump_vec_from_elem_in<T: Testable>() {
-    cfg().max_clones(3).expected_drops(4).run(|| {
+    expected_drops(4).panic_on_clone(3).run(|| {
         let mut bump: Bump = Bump::new();
-        let _vec = MutBumpVec::from_elem_in(Wrap(T::default()), 5, &mut bump);
+        let _vec = MutBumpVec::from_elem_in(T::default(), 5, &mut bump);
     });
 }
 
 fn into_iter<T: Testable>() {
-    cfg().expected_drops(5).expected_msg("whoops").run(|| {
+    expected_drops(5).expected_msg("whoops").run(|| {
         let mut bump: Bump = Bump::new();
-        let vec = mut_bump_vec![in bump; Wrap(T::default()); 5];
+        let vec = mut_bump_vec![in bump; T::default(); 5];
 
         #[allow(clippy::manual_assert)]
         for (i, _) in vec.into_iter().enumerate() {
@@ -103,39 +97,43 @@ fn into_iter<T: Testable>() {
 
 fn mut_bump_vec_extend_from_slice<T: Testable>() {
     let mut bump: Bump = Bump::new();
-    let mut vec: MutBumpVecRev<Wrap<T>> = mut_bump_vec_rev![in bump];
-    let slice = ManuallyDrop::new([
-        Wrap(T::default()),
-        Wrap(T::default()),
-        Wrap(T::default()),
-        Wrap(T::default()),
-        Wrap(T::default()),
-    ]);
+    let mut vec: MutBumpVecRev<T> = mut_bump_vec_rev![in bump];
+    let slice = ManuallyDrop::new(T::array::<5>());
 
-    cfg().max_clones(3).run(|| {
+    expected_drops(0).panic_on_clone(3).run(|| {
         vec.extend_from_slice_clone(&*slice);
     });
 
     assert_eq!(vec.len(), 3);
-
-    // make sure items are initialized
-    // miri will catch that
-    for item in vec {
-        black_box(item);
-    }
+    assert_initialized(vec);
 }
 
-use helper::{cfg, Testable, Wrap};
+use helper::{assert_initialized, expected_drops, Testable};
 
 mod helper {
     use core::{
+        array,
         cell::Cell,
+        hint::black_box,
         panic::{AssertUnwindSafe, RefUnwindSafe, UnwindSafe},
     };
 
-    pub(super) trait Testable: Clone + Default + UnwindSafe + RefUnwindSafe {}
-    impl Testable for i32 {}
-    impl Testable for () {}
+    pub fn assert_initialized(iter: impl IntoIterator) {
+        // make sure items are initialized
+        // miri will catch that
+        for item in iter {
+            black_box(item);
+        }
+    }
+
+    pub(super) trait Testable: Clone + Default + UnwindSafe + RefUnwindSafe {
+        fn array<const N: usize>() -> [Self; N] {
+            array::from_fn(|_| Default::default())
+        }
+    }
+
+    impl Testable for Wrap<i32> {}
+    impl Testable for Wrap<()> {}
 
     thread_local! {
         static MAX_CLONES: Cell<usize> = const { Cell::new(0) };
@@ -147,22 +145,17 @@ mod helper {
     pub(super) struct Cfg {
         max_clones: Option<usize>,
         expected_drops: usize,
-        msg: Option<&'static str>,
+        expected_msg: Option<&'static str>,
     }
 
     impl Cfg {
-        pub(super) fn max_clones(mut self, amount: usize) -> Self {
+        pub(super) fn panic_on_clone(mut self, amount: usize) -> Self {
             self.max_clones = Some(amount);
             self
         }
 
-        pub(super) fn expected_drops(mut self, amount: usize) -> Self {
-            self.expected_drops = amount;
-            self
-        }
-
         pub(super) fn expected_msg(mut self, msg: &'static str) -> Self {
-            self.msg = Some(msg);
+            self.expected_msg = Some(msg);
             self
         }
 
@@ -170,7 +163,7 @@ mod helper {
             let Self {
                 max_clones,
                 expected_drops,
-                msg,
+                expected_msg: msg,
             } = self;
             let msg = msg.unwrap_or("too many clones");
 
@@ -185,10 +178,15 @@ mod helper {
         }
     }
 
-    pub(super) fn cfg() -> Cfg {
-        Cfg::default()
+    pub(super) fn expected_drops(amount: usize) -> Cfg {
+        Cfg {
+            expected_drops: amount,
+            expected_msg: None,
+            max_clones: None,
+        }
     }
 
+    #[derive(Default)]
     pub(super) struct Wrap<T>(pub(super) T);
 
     impl<T: UnwindSafe> UnwindSafe for Wrap<T> {}
