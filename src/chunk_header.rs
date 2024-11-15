75c7786e352a85aa93a7df733f515c2ad0d02370
bluurryy <164359728+bluurryy@users.noreply.github.com>
2024-11-15 20:35:09 +0100

    refactor: default chunk header parameter `A = ()`

--- src/chunk_header.rs
+++ src/chunk_header.rs
@@ -2,7 +2,7 @@ use crate::polyfill::{cfg_const, nonnull};
 use core::{cell::Cell, ptr::NonNull};
 
 #[repr(C, align(16))]
-pub(crate) struct ChunkHeader<A> {
+pub(crate) struct ChunkHeader<A = ()> {
     pub(crate) pos: Cell<NonNull<u8>>,
     pub(crate) end: NonNull<u8>,
 
@@ -14,7 +14,7 @@ pub(crate) struct ChunkHeader<A> {
 
 /// Wraps a [`ChunkHeader`], making it Sync so it can be used as a static.
 /// The empty chunk is never mutated, so this is fine.
-struct UnallocatedChunkHeader(ChunkHeader<()>);
+struct UnallocatedChunkHeader(ChunkHeader);
 
 unsafe impl Sync for UnallocatedChunkHeader {}
 
@@ -28,7 +28,7 @@ static UNALLOCATED_CHUNK_HEADER: UnallocatedChunkHeader = UnallocatedChunkHeader
 
 cfg_const! {
     #[cfg_const(feature = "nightly-const-refs-to-static")]
-    pub(crate) fn unallocated_chunk_header() -> NonNull<ChunkHeader<()>> {
+    pub(crate) fn unallocated_chunk_header() -> NonNull<ChunkHeader> {
         nonnull::from_ref(&UNALLOCATED_CHUNK_HEADER.0)
     }
 }
