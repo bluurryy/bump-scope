42e41249198ac6e2b2ae731ff7ccf9436f1925b2
bluurryy <164359728+bluurryy@users.noreply.github.com>
2024-11-21 03:24:19 +0100

    refactor: moved `NoDrop` to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -297,6 +297,7 @@ mod mut_bump_string;
 pub mod mut_bump_vec;
 /// Contains [`MutBumpVecRev`] and associated types.
 mod mut_bump_vec_rev;
+mod no_drop;
 /// Contains types associated with owned slices.
 pub mod owned_slice;
 /// Contains types associated with owned strings.
@@ -348,6 +349,7 @@ pub use mut_bump_string::MutBumpString;
 #[doc(inline)]
 pub use mut_bump_vec::MutBumpVec;
 pub use mut_bump_vec_rev::MutBumpVecRev;
+pub use no_drop::NoDrop;
 #[cfg(feature = "panic-on-alloc")]
 use private::{capacity_overflow, format_trait_error, Infallibly};
 use set_len_on_drop::SetLenOnDrop;
@@ -360,19 +362,6 @@ pub use without_dealloc::{WithoutDealloc, WithoutShrink};
 const CHUNK_ALIGN_MIN: usize = 16;
 
 const _: () = assert!(CHUNK_ALIGN_MIN == bumping::MIN_CHUNK_ALIGN);
-
-/// This trait marks types that don't need dropping.
-///
-/// This trait is a best effort for modeling such a constraint. It is not implemented for all types that don't need dropping.
-///
-/// Every `T where T: Copy` and every `[T] where T: NoDrop` automatically implements `NoDrop`.
-///
-/// It is used as a bound for [`BumpBox`]'s [`into_ref`](BumpBox::into_ref) and [`into_mut`](BumpBox::into_mut) so you don't accidentally omit a drop that does matter.
-pub trait NoDrop {}
-
-impl NoDrop for str {}
-impl<T: Copy> NoDrop for T {}
-impl<T: NoDrop> NoDrop for [T] {}
 
 /// Specifies the current minimum alignment of a bump allocator.
 #[derive(Clone, Copy)]
---
+++ src/no_drop.rs
@@ -0,0 +1,12 @@
+/// This trait marks types that don't need dropping.
+///
+/// This trait is a best effort for modeling such a constraint. It is not implemented for all types that don't need dropping.
+///
+/// Every `T where T: Copy` and every `[T] where T: NoDrop` automatically implements `NoDrop`.
+///
+/// It is used as a bound for [`BumpBox`]'s [`into_ref`](BumpBox::into_ref) and [`into_mut`](BumpBox::into_mut) so you don't accidentally omit a drop that does matter.
+pub trait NoDrop {}
+
+impl NoDrop for str {}
+impl<T: Copy> NoDrop for T {}
+impl<T: NoDrop> NoDrop for [T] {}
