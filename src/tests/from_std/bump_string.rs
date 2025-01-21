1b7967266ea54f7e2de0b6b32a35acb8d3cc0abe
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-01-21 17:23:41 +0100

    test: update `tests/from_std/bump_string.rs`

--- src/tests/from_std/bump_string.rs
+++ src/tests/from_std/bump_string.rs
@@ -1,3 +1,5 @@
+// Adapted from rust's `library/alloc/tests/string.rs` commit c682aa162b0d41e21cc6748f4fecfe01efb69d1f
+
 use std::{
     assert_matches::assert_matches,
     cell::Cell,
@@ -374,6 +376,81 @@ fn remove_bad() {
     BumpString::from_str_in("ศ", &bump).remove(1);
 }
 
+/*
+TODO
+
+#[test]
+fn test_remove_matches() {
+    // test_single_pattern_occurrence
+    let mut s = "abc".to_string();
+    s.remove_matches('b');
+    assert_eq!(s, "ac");
+    // repeat_test_single_pattern_occurrence
+    s.remove_matches('b');
+    assert_eq!(s, "ac");
+
+    // test_single_character_pattern
+    let mut s = "abcb".to_string();
+    s.remove_matches('b');
+    assert_eq!(s, "ac");
+
+    // test_pattern_with_special_characters
+    let mut s = "ศไทย中华Việt Nam; foobarศ".to_string();
+    s.remove_matches('ศ');
+    assert_eq!(s, "ไทย中华Việt Nam; foobar");
+
+    // test_pattern_empty_text_and_pattern
+    let mut s = "".to_string();
+    s.remove_matches("");
+    assert_eq!(s, "");
+
+    // test_pattern_empty_text
+    let mut s = "".to_string();
+    s.remove_matches("something");
+    assert_eq!(s, "");
+
+    // test_empty_pattern
+    let mut s = "Testing with empty pattern.".to_string();
+    s.remove_matches("");
+    assert_eq!(s, "Testing with empty pattern.");
+
+    // test_multiple_consecutive_patterns_1
+    let mut s = "aaaaa".to_string();
+    s.remove_matches('a');
+    assert_eq!(s, "");
+
+    // test_multiple_consecutive_patterns_2
+    let mut s = "Hello **world****today!**".to_string();
+    s.remove_matches("**");
+    assert_eq!(s, "Hello worldtoday!");
+
+    // test_case_insensitive_pattern
+    let mut s = "CASE ** SeNsItIvE ** PaTtErN.".to_string();
+    s.remove_matches("sEnSiTiVe");
+    assert_eq!(s, "CASE ** SeNsItIvE ** PaTtErN.");
+
+    // test_pattern_with_digits
+    let mut s = "123 ** 456 ** 789".to_string();
+    s.remove_matches("**");
+    assert_eq!(s, "123  456  789");
+
+    // test_pattern_occurs_after_empty_string
+    let mut s = "abc X defXghi".to_string();
+    s.remove_matches("X");
+    assert_eq!(s, "abc  defghi");
+
+    // test_large_pattern
+    let mut s = "aaaXbbbXcccXdddXeee".to_string();
+    s.remove_matches("X");
+    assert_eq!(s, "aaabbbcccdddeee");
+
+    // test_pattern_at_multiple_positions
+    let mut s = "Pattern ** found ** multiple ** times ** in ** text.".to_string();
+    s.remove_matches("**");
+    assert_eq!(s, "Pattern  found  multiple  times  in  text.");
+}
+*/
+
 #[test]
 #[cfg_attr(not(panic = "unwind"), ignore = "test requires unwinding support")]
 fn test_retain() {
@@ -665,7 +742,6 @@ fn test_into_boxed_str() {
 
 #[test]
 fn test_reserve() {
-    // This is all the same as test_reserve
     let bump: Bump = Bump::new();
     let mut s = BumpString::new_in(&bump);
     assert_eq!(s.capacity(), 0);
@@ -685,6 +761,42 @@ fn test_reserve() {
 
     s.reserve(16);
     assert!(s.capacity() >= 33)
+}
+
+#[test]
+fn test_reserve_exact() {
+    // This is all the same as test_reserve
+    let bump: Bump = Bump::new();
+    let mut s = BumpString::new_in(&bump);
+    assert_eq!(s.capacity(), 0);
+
+    s.reserve_exact(2);
+    assert!(s.capacity() >= 2);
+
+    for _i in 0..16 {
+        s.push('0');
+    }
+
+    assert!(s.capacity() >= 16);
+    s.reserve_exact(16);
+    assert!(s.capacity() >= 32);
+
+    s.push('0');
+
+    s.reserve_exact(16);
+    assert!(s.capacity() >= 33)
+}
+
+#[test]
+#[cfg_attr(miri, ignore)] // Miri does not support signalling OOM
+fn test_try_with_capacity() {
+    let bump: Bump = Bump::new();
+
+    let string = BumpString::try_with_capacity_in(1000, &bump).unwrap();
+    assert_eq!(0, string.len());
+    assert!(string.capacity() >= 1000 && string.capacity() <= isize::MAX as usize);
+
+    assert!(BumpString::try_with_capacity_in(usize::MAX, &bump).is_err());
 }
 
 #[test]
