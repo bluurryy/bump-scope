4b572b42d272400cfb4d0daf8353730743c53160
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-05-19 00:40:39 +0200

    test: remove `cfg` in `compile_fail` tests

--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -1,7 +1,3 @@
-// I don't understand what's going on but trybuild doesn't work with that feature.
-// It errors because of a missing `feature(allocator_api)` in hashbrown.
-#![cfg(not(feature = "nightly-allocator-api"))]
-
 #[cfg_attr(miri, ignore)]
 #[test]
 fn mustnt_compile() {
