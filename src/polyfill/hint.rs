195d8a688d4df02ea6928d093e07e197efb8c301
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-05-20 21:27:19 +0200

    move hints in `other` to `hint`

--- src/polyfill/hint.rs
+++ src/polyfill/hint.rs
@@ -4,3 +4,35 @@ pub unsafe fn assert_unchecked(condition: bool) {
         unsafe { core::hint::unreachable_unchecked() }
     }
 }
+
+/// Not part of std.
+#[cold]
+#[inline(always)]
+#[allow(dead_code)]
+pub(crate) fn cold() {}
+
+/// Not part of std.
+#[inline(always)]
+#[allow(dead_code)]
+pub(crate) fn likely(condition: bool) -> bool {
+    if condition {
+        // ...
+    } else {
+        cold();
+    }
+
+    condition
+}
+
+/// Not part of std.
+#[inline(always)]
+#[allow(dead_code)]
+pub(crate) fn unlikely(condition: bool) -> bool {
+    if condition {
+        cold();
+    } else {
+        // ...
+    }
+
+    condition
+}
--- src/polyfill/mod.rs
+++ src/polyfill/mod.rs
@@ -11,46 +11,15 @@ pub(crate) mod slice;
 pub(crate) mod str;
 pub(crate) mod usize;
 
-#[allow(dead_code)]
-mod other {
-    #[inline(always)]
-    pub(crate) const fn const_unwrap<T: Copy>(option: Option<T>) -> T {
-        match option {
-            Some(value) => value,
-            None => panic!("expected Some"),
-        }
-    }
-
-    #[cold]
-    #[inline(always)]
-    pub(crate) fn cold() {}
-
-    #[inline(always)]
-    pub(crate) fn likely(condition: bool) -> bool {
-        if condition {
-            // ...
-        } else {
-            cold();
-        }
-
-        condition
-    }
-
-    #[inline(always)]
-    pub(crate) fn unlikely(condition: bool) -> bool {
-        if condition {
-            cold();
-        } else {
-            // ...
-        }
-
-        condition
-    }
-}
-
 use core::mem::{size_of, ManuallyDrop};
 
-pub(crate) use other::*;
+#[inline(always)]
+pub(crate) const fn const_unwrap<T: Copy>(option: Option<T>) -> T {
+    match option {
+        Some(value) => value,
+        None => panic!("expected Some"),
+    }
+}
 
 pub(crate) unsafe fn transmute_value<A, B>(a: A) -> B {
     assert!(size_of::<A>() == size_of::<B>());
