821e8aafa779794092e37ca7edf3b76e2deb1502
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-05-20 21:30:16 +0200

    improve const_unwrap impl

--- src/polyfill/option.rs
+++ src/polyfill/option.rs
@@ -2,6 +2,13 @@
 pub(crate) const fn const_unwrap<T: Copy>(option: Option<T>) -> T {
     match option {
         Some(value) => value,
-        None => panic!("expected Some"),
+        None => unwrap_failed(),
     }
+}
+
+#[cold]
+#[inline(never)]
+#[track_caller]
+const fn unwrap_failed() -> ! {
+    panic!("called `Option::unwrap()` on a `None` value")
 }
