13026b8c2a93f73671c494124a60b32d22608b3b
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-06-07 08:33:35 +0200

    add callgrind based benches

---
+++ crates/callgrind-benches/README.md
@@ -0,0 +1,35 @@
+# Benchmarks
+
+instructions / branches
+
+| name                 | bump-scope | bumpalo  | blink-alloc |
+| -------------------- | ---------- | -------- | ----------- |
+| alloc_u8             | 10 / 1     | 11 / 2   | 16 / 4      |
+| alloc_u32            | 14 / 1     | 15 / 3   | 18 / 4      |
+| alloc_u32_try        | 14 / 1     | 15 / 3   | 18 / 4      |
+| allocate_u32         | 15 / 2     | 13 / 3   | 16 / 4      |
+| allocate             | 16 / 2     | 26 / 5   | 23 / 4      |
+| grow_same_align      | 19 / 2     | 53 / 4   | 18 / 4      |
+| grow_smaller_align   | 19 / 2     | 53 / 4   | 18 / 4      |
+| grow_larger_align    | 19 / 2     | 17 / 3   | 20 / 4      |
+| shrink_same_align    | 11 / 2     | 12 / 1   | 5 / 1       |
+| shrink_smaller_align | 11 / 2     | 12 / 1   | 5 / 1       |
+| shrink_larger_align  | 11 / 2     | 5 / 1    | 20 / 4      |
+| warm_up              | 227 / 31   | 363 / 43 | 284 / 38    |
+| reset                | 26 / 2     | 23 / 2   | 26 / 3      |
+
+# Reproducing
+
+[Valgrind](https://valgrind.org/) must be installed and its header files accessible. If you have installed `Valgrind` using OS-specific package manager, the paths to the headers are likely to be resolved automatically by [`cc`](https://docs.rs/cc/latest/cc/index.html).
+
+In case of manual installation, you can set the path to the `Valgrind` headers location through the `DEP_VALGRIND` environment variable. For example:
+
+```bash
+DEP_VALGRIND=/home/linuxbrew/.linuxbrew/include cargo build --benches
+```
+
+Now just write
+```bash
+cargo bench
+```
+and the markdown formatted table seen above will be printed to stdout.
---
+++ crates/callgrind-benches/benches/benches.rs
@@ -0,0 +1,493 @@
+use core::fmt;
+use std::alloc::Layout;
+use std::ptr::NonNull;
+
+use calliper::{Report, Runner, Scenario, ScenarioConfig};
+
+use allocator_api2::alloc::{AllocError, Allocator};
+use indexmap::IndexMap;
+use markdown_tables::MarkdownTableRow;
+
+trait CommonBehavior {
+    fn new() -> Self;
+    fn with_capacity(capacity: usize) -> Self;
+    fn alloc<T>(&self, value: T) -> &T;
+    fn try_alloc<T>(&self, value: T) -> Option<&T>;
+    fn as_allocator(&self) -> impl Allocator;
+    fn reset(&mut self);
+}
+
+impl CommonBehavior for bump_scope::Bump {
+    #[inline(always)]
+    fn new() -> Self {
+        bump_scope::Bump::new()
+    }
+
+    #[inline(always)]
+    fn with_capacity(capacity: usize) -> Self {
+        bump_scope::Bump::with_size(capacity)
+    }
+
+    #[inline(always)]
+    fn alloc<T>(&self, value: T) -> &T {
+        bump_scope::BumpBox::leak(self.alloc(value))
+    }
+
+    #[inline(always)]
+    fn try_alloc<T>(&self, value: T) -> Option<&T> {
+        match self.try_alloc(value) {
+            Ok(value) => Some(bump_scope::BumpBox::leak(value)),
+            Err(_) => None,
+        }
+    }
+
+    #[inline(always)]
+    fn as_allocator(&self) -> impl Allocator {
+        self
+    }
+
+    #[inline(always)]
+    fn reset(&mut self) {
+        self.reset();
+    }
+}
+
+impl CommonBehavior for bumpalo::Bump {
+    #[inline(always)]
+    fn new() -> Self {
+        bumpalo::Bump::new()
+    }
+
+    #[inline(always)]
+    fn with_capacity(capacity: usize) -> Self {
+        bumpalo::Bump::with_capacity(capacity)
+    }
+
+    #[inline(always)]
+    fn alloc<T>(&self, value: T) -> &T {
+        self.alloc(value)
+    }
+
+    #[inline(always)]
+    fn try_alloc<T>(&self, value: T) -> Option<&T> {
+        match self.try_alloc(value) {
+            Ok(value) => Some(value),
+            Err(_) => None,
+        }
+    }
+
+    #[inline(always)]
+    fn as_allocator(&self) -> impl Allocator {
+        self
+    }
+
+    #[inline(always)]
+    fn reset(&mut self) {
+        self.reset();
+    }
+}
+
+impl CommonBehavior for blink_alloc::Blink {
+    #[inline(always)]
+    fn new() -> Self {
+        blink_alloc::Blink::new()
+    }
+
+    #[inline(always)]
+    fn with_capacity(capacity: usize) -> Self {
+        let this = blink_alloc::Blink::with_chunk_size(capacity);
+        // Blink does not allocate a chunk on creation.
+        // We allocate here to make sure a chunk is allocated to make it fair.
+        _ = this.allocator().allocate(Layout::new::<[u64; 2]>()).ok();
+        this
+    }
+
+    #[inline(always)]
+    fn alloc<T>(&self, value: T) -> &T {
+        self.put_no_drop(value)
+    }
+
+    #[inline(always)]
+    fn try_alloc<T>(&self, value: T) -> Option<&T> {
+        match self.emplace_no_drop().try_value(value) {
+            Ok(value) => Some(value),
+            Err(_) => None,
+        }
+    }
+
+    #[inline(always)]
+    fn as_allocator(&self) -> impl Allocator {
+        self.allocator()
+    }
+
+    #[inline(always)]
+    fn reset(&mut self) {
+        self.reset();
+    }
+}
+
+macro_rules! scenario {
+    ($name:ident, $library:ident) => {
+        paste::paste! {
+            crate::scenario([<wrapper_ $name _ $library>], stringify!([<$name _ $library>]))
+        }
+    };
+}
+
+macro_rules! library {
+    (
+        bench: $bench:ident,
+        library: $library:ident,
+        implementor: $implementor:ty,
+        params: ($($param:ident: $param_ty:ty),*) $(-> $ret:ty)?,
+    ) => {
+        paste::paste! {
+            pub mod $library {
+                #[allow(unused_imports)]
+                use crate::*;
+
+                type Bump = $implementor;
+
+                #[inline(never)]
+                #[unsafe(no_mangle)]
+                pub fn [<wrapper_ $bench _ $library>]() {
+                    super::generic_wrapper::<Bump>(|$($param: $param_ty),*| {
+                        _ = std::hint::black_box([<$bench _ $library>]($(std::hint::black_box($param)),*));
+                    });
+                }
+
+                #[inline(never)]
+                #[unsafe(no_mangle)]
+                pub fn [<$bench _ $library>]($($param: $param_ty),*) $(-> $ret)? {
+                    super::generic_run::<Bump>($($param),*)
+                }
+            }
+        }
+    };
+}
+
+pub(crate) use library;
+
+macro_rules! bench_impls {
+    (
+        $(
+            $name:ident {
+                wrap($run_f:ident) {
+                    $($wrap:tt)*
+                }
+                run($($param:ident: $param_ty:ty),*) $(-> $ret:ty)? {
+                    $($run:tt)*
+                }
+            }
+        )*
+    ) => {
+        paste::paste! {
+            pub mod bench_impls {
+                $(
+                    pub mod $name {
+                        #[allow(unused_imports)]
+                        use crate::*;
+
+                        #[inline(always)]
+                        fn generic_wrapper<Bump: crate::CommonBehavior>(
+                            $run_f: fn($($param_ty),*),
+                        )  {
+                            $($wrap)*
+                        }
+
+                        #[inline(always)]
+                        fn generic_run<Bump: crate::CommonBehavior>($($param: $param_ty),*) $(-> $ret)? {
+                            $($run)*
+                        }
+
+                        crate::library! {
+                            bench: $name,
+                            library: bump_scope,
+                            implementor: bump_scope::Bump,
+                            params: ($($param: $param_ty),*) $(-> $ret)?,
+                        }
+
+                        crate::library! {
+                            bench: $name,
+                            library: bumpalo,
+                            implementor: bumpalo::Bump,
+                            params: ($($param: $param_ty),*) $(-> $ret)?,
+                        }
+
+                        crate::library! {
+                            bench: $name,
+                            library: blink_alloc,
+                            implementor: blink_alloc::Blink,
+                            params: ($($param: $param_ty),*) $(-> $ret)?,
+                        }
+                    }
+                )*
+
+                pub fn scenarios() -> Vec<calliper::Scenario> {
+                    $(
+                        use $name::bump_scope::*;
+                        use $name::bumpalo::*;
+                        use $name::blink_alloc::*;
+                    )*
+
+                    vec![
+                        $(
+                            scenario!($name, bump_scope),
+                            scenario!($name, bumpalo),
+                            scenario!($name, blink_alloc),
+                        )*
+                    ]
+                }
+            }
+        }
+    };
+}
+
+bench_impls! {
+    alloc_u8 {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            run(&bump, 42);
+        }
+        run(bump: &Bump, value: u8) -> &u8 {
+            bump.alloc(value)
+        }
+    }
+
+    alloc_u32 {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            run(&bump, 42);
+        }
+        run(bump: &Bump, value: u32) -> &u32 {
+            bump.alloc(value)
+        }
+    }
+
+    alloc_u32_try {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            run(&bump, 42);
+        }
+        run(bump: &Bump, value: u32) -> Option<&u32> {
+            bump.try_alloc(value)
+        }
+    }
+
+    allocate_u32 {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            run(&bump);
+        }
+        run(bump: &Bump) -> Result<NonNull<[u8]>, AllocError> {
+            bump.as_allocator().allocate(Layout::new::<u32>())
+        }
+    }
+
+    allocate {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            run(&bump, Layout::new::<u32>());
+        }
+        run(bump: &Bump, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+            bump.as_allocator().allocate(layout)
+        }
+    }
+
+    grow_same_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<u32>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().grow(ptr, Layout::new::<u32>(), Layout::new::<[u32; 2]>()) }
+        }
+    }
+
+    grow_smaller_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<u32>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().grow(ptr, Layout::new::<u32>(), Layout::new::<[u16; 4]>()) }
+        }
+    }
+
+    grow_larger_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<u32>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().grow(ptr, Layout::new::<u32>(), Layout::new::<u64>()) }
+        }
+    }
+
+    shrink_same_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<[u32; 2]>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().shrink(ptr, Layout::new::<[u32; 2]>(), Layout::new::<u32>()) }
+        }
+    }
+
+    shrink_smaller_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<u32>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().shrink(ptr, Layout::new::<u32>(), Layout::new::<u16>()) }
+        }
+    }
+
+    shrink_larger_align {
+        wrap(run) {
+            let bump = Bump::with_capacity(1024);
+            let ptr = bump.as_allocator().allocate(Layout::new::<[u16; 4]>()).unwrap().cast::<u8>();
+            run(&bump, ptr);
+        }
+        run(bump: &Bump, ptr: NonNull<u8>) ->  Result<NonNull<[u8]>, AllocError> {
+            unsafe { bump.as_allocator().shrink(ptr, Layout::new::<[u16; 4]>(), Layout::new::<u32>()) }
+        }
+    }
+
+    warm_up {
+        wrap(run) {
+            run();
+        }
+        run() -> Bump {
+            let bump = Bump::new();
+            bump.alloc(0u32);
+            bump
+        }
+    }
+
+    reset {
+        wrap(run) {
+            let mut bump = Bump::with_capacity(1024);
+            bump.as_allocator().allocate(Layout::new::<u32>()).unwrap();
+            run(&mut bump);
+        }
+        run(bump: &mut Bump) {
+            bump.reset();
+        }
+    }
+}
+
+fn scenario(f: fn(), name: &str) -> Scenario {
+    Scenario::new(f)
+        .name(name)
+        .config(ScenarioConfig::default().filters([name]))
+}
+
+#[derive(Clone, Copy)]
+struct BenchResult {
+    instructions: u64,
+    branches: u64,
+    #[expect(dead_code)]
+    branch_misses: u64,
+}
+
+impl BenchResult {
+    fn new(report: &Report) -> Self {
+        let parsed = report.parse();
+
+        BenchResult {
+            instructions: parsed.instruction_reads.unwrap(),
+            branches: parsed.branches.unwrap_or(0),
+            branch_misses: parsed.branch_misses.unwrap_or(0),
+        }
+    }
+}
+
+impl fmt::Display for BenchResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self {
+            instructions,
+            branches,
+            branch_misses: _,
+        } = self;
+        f.write_fmt(format_args!("{instructions} / {branches}"))
+    }
+}
+
+struct Row {
+    name: String,
+    bump_scope: BenchResult,
+    bumpalo: BenchResult,
+    blink_alloc: BenchResult,
+}
+
+impl MarkdownTableRow for Row {
+    fn column_names() -> Vec<&'static str> {
+        vec!["name", "bump-scope", "bumpalo", "blink-alloc"]
+    }
+
+    fn column_values(&self) -> Vec<String> {
+        vec![
+            self.name.to_string(),
+            self.bump_scope.to_string(),
+            self.bumpalo.to_string(),
+            self.blink_alloc.to_string(),
+        ]
+    }
+}
+
+fn split_name(name: &str) -> [&str; 2] {
+    let library_names = ["bump_scope", "bumpalo", "blink_alloc"];
+
+    for library_name in library_names {
+        if name.ends_with(library_name) {
+            let mid = name.len() - library_name.len();
+            return [&name[..mid - 1], &name[mid..]];
+        }
+    }
+
+    panic!("bench function did not end with a library name suffix")
+}
+
+fn main() {
+    let runner = Runner::default().config(ScenarioConfig::default().branch_sim(true));
+    let scenarios = bench_impls::scenarios();
+
+    let reports = runner
+        .run(&scenarios)
+        .expect("runner failed")
+        .expect("runner didn't return anything");
+
+    for report in &reports {
+        let parsed = report.parse();
+        eprintln!("\n{parsed}");
+    }
+
+    let mut map = IndexMap::<String, IndexMap<String, BenchResult>>::new();
+
+    for report in &reports {
+        let [name, library] = split_name(report.scenario.get_name()).map(String::from);
+        let result = BenchResult::new(report);
+        map.entry(name).or_default().insert(library, result);
+    }
+
+    let mut rows = vec![];
+
+    for (name, libraries) in map {
+        rows.push(Row {
+            name,
+            bump_scope: libraries["bump_scope"],
+            bumpalo: libraries["bumpalo"],
+            blink_alloc: libraries["blink_alloc"],
+        })
+    }
+
+    let table = markdown_tables::as_table(&rows);
+    println!("{table}");
+}
