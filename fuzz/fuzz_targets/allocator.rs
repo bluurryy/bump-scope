9d6e0f4293a5b2aca0ef322aca4dd7f1f9107160
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-06-10 18:17:29 +0200

    try_init

--- fuzz/fuzz_targets/allocator.rs
+++ fuzz/fuzz_targets/allocator.rs
@@ -4,6 +4,6 @@ use fuzzing_support::allocator_api::Fuzz;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|fuzz: Fuzz| {
-    env_logger::init();
+    _ = env_logger::try_init();
     fuzz.run()
 });
