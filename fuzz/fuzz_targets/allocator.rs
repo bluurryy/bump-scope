33ef116cebe5da3d6c5ce0e64da0e7299b9eacfb
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-06-10 17:55:45 +0200

    use env_logger in allocator fuzz target

--- fuzz/fuzz_targets/allocator.rs
+++ fuzz/fuzz_targets/allocator.rs
@@ -3,4 +3,7 @@
 use fuzzing_support::allocator_api::Fuzz;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|fuzz: Fuzz| fuzz.run());
+fuzz_target!(|fuzz: Fuzz| {
+    env_logger::init();
+    fuzz.run()
+});
