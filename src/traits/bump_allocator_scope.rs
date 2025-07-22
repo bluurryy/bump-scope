4523698051335749c5e86fab8294c0675790ac33
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-07-22 23:21:49 +0200

    doc bump allocator scope

--- src/traits/bump_allocator_scope.rs
+++ src/traits/bump_allocator_scope.rs
@@ -7,6 +7,27 @@ use crate::{
     WithoutDealloc, WithoutShrink,
 };
 
+/// A bump allocator scope.
+///
+/// This is a [`BumpAllocator`] which can make allocations that outlive itself.
+/// Specifically, its allocations live for the lifetime `'a`.
+///
+/// # Safety
+///
+/// This trait must only be implemented when allocations live for `'a`.
+/// For example this function must be sound:
+///
+/// ```
+/// # #![allow(dead_code)]
+/// use bump_scope::BumpAllocatorScope;
+/// use core::alloc::Layout;
+///
+/// fn allocate_zeroed_bytes<'a>(allocator: impl BumpAllocatorScope<'a>, len: usize) -> &'a [u8] {
+///     let layout = Layout::array::<u8>(len).unwrap();
+///     let ptr = allocator.allocate_zeroed(layout).unwrap();
+///     unsafe { ptr.as_ref() }
+/// }
+/// ```
 pub unsafe trait BumpAllocatorScope<'a>: BumpAllocator {}
 
 assert_dyn_compatible!(BumpAllocatorScope<'_>);
