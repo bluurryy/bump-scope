14fb7af08284f255b64d783f89321e8836e25884
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-08-07 05:54:30 +0200

    fix check

--- src/polyfill/layout.rs
+++ src/polyfill/layout.rs
@@ -1,10 +1,8 @@
-#[cfg(feature = "alloc")]
 use core::{alloc::Layout, num::NonZeroUsize, ptr::NonNull};
 
 /// See [`std::alloc::Layout::dangling`].
 #[must_use]
 #[inline]
-#[cfg(feature = "alloc")]
 pub(crate) const fn dangling(layout: Layout) -> NonNull<u8> {
     unsafe { super::non_null::without_provenance(NonZeroUsize::new_unchecked(layout.align())) }
 }
