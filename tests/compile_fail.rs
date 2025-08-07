636262ff6d21f7abd6f850ea70274dc300342484
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-08-07 07:23:32 +0200

    run compile fail tests on nightly only

--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -1,4 +1,5 @@
-#[cfg_attr(miri, ignore)]
+// this doesn't require nightly, but nightly can have a different error message
+#[cfg_attr(any(miri, not(feature = "nightly-tests")), ignore)]
 #[test]
 fn mustnt_compile() {
     let t = trybuild::TestCases::new();
